5ba0f58376d5bbdfffdf44e10207d0ab31fe2995
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-09-11 14:27:22 +0530

    method to evaluate arithematic operations

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,12 @@
 def calculate(num1, oper, num2)
-	num1.to_i.send oper, num2.to_i
+	num1.to_f.send oper, num2.to_f
 end
 
-puts "Enter the Expression"
-exp = gets.chomp
-oper = exp.gsub(/[\d]*/,"")
-num1 = exp[0..exp.index(oper)-1]
-num2 = exp[exp.index(oper)+1..exp.length-1]
-oper = oper.to_sym
+puts "Enter fist operand"
+num1 = gets.chomp
+puts "Enter second operand"
+num2 = gets.chomp
+puts "Enter operator"
+oper = gets.chomp.to_sym
+
 puts calculate(num1,oper,num2)
