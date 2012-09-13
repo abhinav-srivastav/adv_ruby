2197396a6cdac00aae4332d3f23a399308795462
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-09-13 11:30:07 +0530

    inherit class string & create dynamic method

---
+++ dynamic_method.rb
@@ -0,0 +1,25 @@
+class Dynamic_method < String
+  def exclude?(other)
+   return nil if self.include? other
+   return true
+  end
+  def reverse_sentence()
+   self.split().reverse.join(" ")
+  end
+  def append_word(other)
+    self.gsub(" "," #{other} ")
+  end
+end
+
+puts "Create an Object as Dynamic_method.new 'string' "
+input = gets.chomp
+obj = eval(input)
+puts "Methods avialable to execute are :"
+Dynamic_method.instance_methods(false).each do |func|
+  args = Dynamic_method.instance_method(func).arity
+  puts "#{func}(##{args} string args)"
+end
+input = "obj."+gets.chomp
+Dynamic_method.instance_eval do
+ puts eval(input)  
+end
